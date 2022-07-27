aed0a21bb7504d9314cee62161eb6ceff45e3e2d
Dmitry <sheklein.dima@gmail.com>
2022-07-27 11:07:34 +0300

    Воводяться объявления на карте

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
 import FormState from './form.js';
+import { generateCard, generateData } from './generate.js';
+import { OFFERS_LENGTH } from './data.js';
 
 const formElements = document.querySelectorAll('.ad-form, .map__filters');
 formElements.forEach((form) => new FormState(form).disabled());
@@ -25,25 +27,52 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-const mainPinIcon = L.icon({
-  iconUrl: './img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
-const marker = L.marker(
+const PinIcons = {
+  MAIN: L.icon({
+    iconUrl: './img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
+  }),
+  DEFAULT: L.icon({
+    iconUrl: './img/pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  }),
+};
+const markerGroup = L.layerGroup().addTo(map);
+const createMarker = (point) => {
+  const {
+    location: { lat, lng },
+  } = point;
+  const marker = L.marker(
+    {
+      lat,
+      lng,
+    },
+    {
+      icon: PinIcons.DEFAULT,
+    },
+  );
+
+  marker.addTo(markerGroup).bindPopup(generateCard(point));
+};
+const points = generateData(OFFERS_LENGTH);
+points.forEach(createMarker);
+
+const mainMarker = L.marker(
   {
     lat: TokioCoordinate.lat,
     lng: TokioCoordinate.lng,
   },
   {
     draggable: true,
-    icon: mainPinIcon,
+    icon: PinIcons.MAIN,
   },
 );
 
-marker.addTo(map);
-marker.on('moveend', (evt) => {
-  const currentCoordinate = evt.target.getLatLng();
+mainMarker.addTo(map);
+mainMarker.on('moveend', ({ target }) => {
+  const currentCoordinate = target.getLatLng();
 
   addressInput.value = Object.values(currentCoordinate)
     .map((el) => el.toFixed(5))
