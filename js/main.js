159760cc118d69ea0104e833cd76ae033bd4bfe0
Dmitry <sheklein.dima@gmail.com>
2022-07-27 11:05:19 +0300

    Удалиться вывод рандомных объявлений

--- js/main.js
+++ js/main.js
@@ -1,13 +1,2 @@
-import { generateData, generateCard } from './generate.js';
-import { OFFERS_LENGTH } from './data.js';
 import './form-validation.js';
 import './map.js';
-
-const container = document.querySelector('#map-canvas');
-const offersArray = generateData(OFFERS_LENGTH);
-
-offersArray.forEach((offer) => {
-  const cardElement = generateCard(offer);
-
-  container.appendChild(cardElement);
-});
